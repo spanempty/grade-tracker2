b12423bd2b779b3dbaba0a04ca8b2e5c3f07461a
spanempty <spanempty@gmail.com>
2025-04-01 00:03:22 +0300

    theme toggler is added

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -1,4 +1,4 @@
-export default function Ai() {
+export function Ai() {
   return (
     <div className="grid grid-rows-[1fr] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-neutral-100 dark:bg-neutral-950">
       <main className="flex flex-col gap-[32px] row-start-1 items-center sm:items-start">
